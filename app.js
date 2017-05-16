3effcdfa4436542ed9ae938bb7b7740a3a68ec6f
Nick <plus0bit@gmail.com>
2017-05-16 21:59:29 +0300

    Init Commit

---
+++ app.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var app = express();
+
+app.set('view engine', 'ejs');
+
+var routes = require('./routes');
+
+var path = require('path');
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+// Routes
+
+// home
+app.get('/', routes.home);
+
+// movie_single
+app.get('/star_wars_episode/:episode_number?', routes.movie_single);
+
+// notFound
+
+app.get('*', routes.notFound);
+
+
+app.listen(3000, function(){
+  console.log("The application is running on port 3000...");
+});
