f2da1bed9adb86e797dcc1d02a0c990537258b3d
Nick <plus0bit@gmail.com>
2017-05-17 14:03:27 +0300

    Add a description to episode

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var moviesJSON = require('../movies.json');
 
 // home
 exports.home = function(req, res){
+
   var movies = moviesJSON.movies;
 
   res.render('home', {
@@ -13,8 +14,24 @@ exports.home = function(req, res){
 
 // movie_single
 exports.movie_single = function(req, res){
+
   var episode_number = req.params.episode_number;
-  res.send("This is the page for episode " + episode_number);
+
+  var movies = moviesJSON.movies;
+
+  if (episode_number >= 1 && episode_number <= 6) {
+    var movie = movies[episode_number -1];
+
+    var title = movie.title;
+
+    res.render('movie_single', {
+      movies : movies,
+      title : title,
+      movie : movie
+    });
+  } else {
+    res.send("This is not the page you are looking for.");
+  }
 };
 
 // notFound
